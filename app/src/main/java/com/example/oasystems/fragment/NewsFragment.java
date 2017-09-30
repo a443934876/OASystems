f2b5416e8038308f8fb8b8a889d58f642699ddfa
a443934876 <378163883@qq.com>
2017-09-30 12:39:41 +0800

    更新

--- app/src/main/java/com/example/oasystems/fragment/NewsFragment.java
+++ app/src/main/java/com/example/oasystems/fragment/NewsFragment.java
@@ -3,10 +3,16 @@ package com.example.oasystems.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.oasystems.R;
 import com.example.oasystems.utils.Inform;
@@ -27,7 +33,29 @@ public class NewsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View activity_news = inflater.inflate(R.layout.activity_news,container,false);
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar_news);
+//        toolbar.setTitle("");
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
         return activity_news;
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.toolbar,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add:
+                Toast.makeText(getActivity(),"选中Add",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 
     @Override
