6579a397818e36920dfa657ad24555c4a7175223
a443934876 <378163883@qq.com>
2017-10-23 15:57:15 +0800

    更新

---
+++ app/src/main/java/com/example/oasystems/fragment/CommonFragment.java
@@ -0,0 +1,42 @@
+package com.example.oasystems.fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.oasystems.R;
+import com.example.oasystems.utils.Inform;
+import com.example.oasystems.utils.InformAdapter;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
+
+/**
+ * 项目名称：OASystems
+ * 创建时间：2017/10/23 15:41
+ * 注释说明：
+ */
+
+public    class CommonFragment extends Fragment   {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_common_fragment,container,false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ListView listView = (ListView) getActivity().findViewById(R.id.listView_inform);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        List<Inform> informList = DataSupport.findAll(Inform.class);
+        InformAdapter adapter = new InformAdapter(informList, inflater);
+        listView.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/oasystems/fragment/NewsFragment.java
+++ app/src/main/java/com/example/oasystems/fragment/NewsFragment.java
@@ -3,26 +3,12 @@ package com.example.oasystems.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.oasystems.R;
-import com.example.oasystems.utils.Inform;
-import com.example.oasystems.utils.InformAdapter;
-
-import org.litepal.crud.DataSupport;
-
-import java.util.List;
 
 /**
  * 项目名称：OASystems
@@ -43,10 +29,17 @@ public class NewsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        ListView listView = (ListView) getActivity().findViewById(R.id.listView_inform);
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        List<Inform> informList = DataSupport.findAll(Inform.class);
-        InformAdapter adapter = new InformAdapter(informList, inflater);
-        listView.setAdapter(adapter);
+//        ListView listView = (ListView) getActivity().findViewById(R.id.listView_inform);
+//        LayoutInflater inflater = getActivity().getLayoutInflater();
+//        List<Inform> informList = DataSupport.findAll(Inform.class);
+//        InformAdapter adapter = new InformAdapter(informList, inflater);
+//        listView.setAdapter(adapter);
+        FragmentTabHost fragmentTabHost= (FragmentTabHost)getActivity().findViewById(R.id.tab_host);
+        fragmentTabHost.setup(getActivity(),getFragmentManager(),R.id.fragment);
+        fragmentTabHost.addTab(fragmentTabHost.newTabSpec("common").setIndicator("普通"),CommonFragment.class,null);
+
+
     }
+
+
 }
